455f444467092a1341ebdede01ad960c7063c1be
Abhay Mathur <a21.mathur21@gmail.com>
2023-10-29 12:08:12 +0530

    Added Chatbot

---
+++ Chatbot/Chatbot.py
@@ -0,0 +1,37 @@
+import openai
+import streamlit as st
+
+
+with st.sidebar:
+    
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()  # This is to load the .env file
+    openai_api_key = os.getenv("OPEN_API_KEY")  # This is to get the API key from the .env file
+    
+
+    # "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    # "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("LegalNinja Chatbot")
+st.caption("🚀 A Chatbot Powered by OpenAI LLM for overall education on Legal Knowledge")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
---
+++ Chatbot/README.md
@@ -0,0 +1,3 @@
+After running the command 'pip install -r requirements.txt' run the following command to run the Chat Bot:
+
+streamlit run Chatbot.py
